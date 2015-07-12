74ae22fd2b001482c205f02a7541783931a44cac
EthanShimooka <sshimook@ucsc.edu>
2015-07-12 03:22:18 -0700

    Merge branch 'feature_visual' of https://github.com/EthanShimooka/sgsling into feature_visual
    Conflicts:
    	index.html

--- js/visualObject.js
+++ js/visualObject.js
@@ -9,13 +9,14 @@
 // Takes a radius and a set of coordinates, converts to a Three.Vector3 Object, and maps it, with radius to a
 // THREE.Sphere object. Returns THREE.Sphere Object.
 
-function visualObjectSphere(radius, x, y, z, scene){
+function visualObjectSphere(radius, x, y, z, scene, colors){
 
     this.x = x;
     this.y = y;
     this.z =  z;
     this.radius = radius;
     this.scene = scene;
+    this.colors = colors;
 
     var scale = 10; //vertex optimization fornula here
     var sgeometry = new THREE.SphereGeometry(radius,scale ,scale);
@@ -24,15 +25,113 @@ function visualObjectSphere(radius, x, y, z, scene){
 
     sphereA.position.set(x,y,z);
 
-    scene.add(sphere);
+    GL.scene.add(sphereA);
 
-    return this;
+}
+
+//visualObjectSmokeStack()
+//draws chimnay at x, and y
+
+function visualObjectSmokeStack(x,y,z, scene) {
+
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+    this.scene = scene;
+
+    var scale = 10; //vertex optimization fornula here
+    var sgeometry = new THREE.CylinderGeometry(2, 6, 40, 52);
+    var smaterial = new THREE.MeshPhongMaterial({
+        color: 0xCC6699,
+        specular: 0xffffff,
+        shininess: 5,
+        shading: THREE.FlatShading
+        //map: texture,
+        //  specularMap: spectexture
+    });
+
+    var smoke = new THREE.Mesh(sgeometry, smaterial);
+    smoke.position.set(x, y, z);
+
+    GL.scene.add(smoke);
 }
 
 
+function visualObjectBuilding(x,y,z,scene){
 
 
+    this.x = x;
+    this.y = y;
+    this.z = z;
 
+    this.scene = scene;
+    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
+    var smaterial = new THREE.MeshPhongMaterial({
+        color: 0xCC6699,
+        specular: 0xffffff,
+        shininess: 5,
+        shading: THREE.FlatShading
+        //map: texture,
+        //  specularMap: spectexture
+    });
+    var cube = new THREE.Mesh( geometry, smaterial );
+    cube.position.set(x+3,y,z);
+
+    GL.scene.add( cube );
+}
+
+
+//visualObjectGrid() -- draws grid mesh
+//
+
+function visualObjectGrid() {
+
+    var texture = THREE.ImageUtils.loadTexture(
+        'textures/patterns/yellowchecker.png'
+    );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat = new THREE.Vector2(50, 50);
+    texture.anisotropy = renderer.getMaxAnisotropy();
+
+    spectexture = THREE.ImageUtils.loadTexture(
+        'textures/patterns/blacklinegrid.png'
+    );
+    spectexture.wrapS = THREE.RepeatWrapping;
+    spectexture.wrapT = THREE.RepeatWrapping;
+    spectexture.repeat = new THREE.Vector2(50, 50);
+    spectexture.anisotropy = renderer.getMaxAnisotropy();
+
+
+    var material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        specular: 0xffffff,
+        shininess: 5,
+        shading: THREE.FlatShading,
+        map: texture,
+        specularMap: spectexture
+    });
+
+    var geometry = new THREE.PlaneGeometry(1000, 1000);
+
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = -Math.PI / 2;
+    GL.scene.add(mesh);
+
+}
+
+function visualObjectLighting() {
+
+    var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
+    GL.scene.add(light);
+}
+
+function visualObjectPointLighting() {
+    var objlight = new THREE.PointLight(0xffffff, 0.7);
+    objlight.position.set(0, 50, 70);
+    GL.scene.add(objlight);
+}
 
 
 
