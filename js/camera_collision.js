dafe5904c594117442c766b659a8771c651afc45
Hunter S <dook@stardust.red>
2015-07-12 14:56:49 -0400

    Hovering

--- js/camera_collision.js
+++ js/camera_collision.js
@@ -1,5 +1,6 @@
 function RayCamera() {
   this.raycaster = new THREE.Raycaster();
+  this.raycaster.precision = 100.00;
   this.raycaster.setFromCamera(new THREE.Vector2(0, 0), GL.camera);
   this.arrow = new THREE.ArrowHelper(
     this.raycaster.ray.direction,
@@ -7,9 +8,8 @@ function RayCamera() {
     100, 0xFFFFFF
   );
   GL.scene.add(this.arrow);
-  this.colliders = [];
 
-  this.active_collision = false;
+  this.colliders = [];
   this.active_collider  = undefined;
   return this;
 }
@@ -30,20 +30,13 @@ RayCamera.prototype.delCollider = function(mesh) {
 
 RayCamera.prototype.checkCollisions = function() {
   var collisions = this.raycaster.intersectObjects(this.colliders);
+
   if(collisions.length > 0) {
     // Only set first collision
-    this.active_collider   = collisions[0];
-    this.active_collision  = true;
+    this.active_collider = collisions[0];
+    this.active_collider.selected = true;
   } else {
-    this.active_collider    = null;
-    this.active_collision  = false;
-  }
-
-  if(this.active_collision) {
-    // RayCamera was already pointing at an object
-    if(this.active_collider === null) {
-      this.active_collider  = null; // Deselect object
-      this.active_collision = false;
-    }
+    try { this.active_collider.selected= false; } catch(e) {}
+    this.active_collider = null;
   }
 };
